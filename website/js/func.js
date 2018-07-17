6990522a04fb0eed68b6afc6ff2d22494bf20650
IMintern <41299891+IMintern@users.noreply.github.com>
2018-07-17 11:19:47 +0100

    Update func.js

--- website/js/func.js
+++ website/js/func.js
@@ -1,1 +1,24 @@
-console.log('hey ninjas!')
+console.log('hey ninjas!');
+
+function whenLoaded() {
+
+var el = document.getElementById('switch');
+
+if(el){
+	console.log('el')
+  el.addEventListener("click", changeCSS)
+} else {
+	console.log('noel')
+}
+
+function changeCSS() {
+	if (document.getElementById('css').href === "http://localhost:8080/css/style.css") {
+		document.getElementById('css').href = "http://localhost:8080/css/other.css";
+	} else {
+		document.getElementById('css').href = "http://localhost:8080/css/style.css"
+	}
+}
+
+}
+
+window.onload = whenLoaded;
